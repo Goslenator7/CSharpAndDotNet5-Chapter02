c711fe3bac82ac3f6b32fa8ca8537aa88e8c9f48
ryan <ryancumming7@gmail.com>
2022-02-10 16:28:37 +0000

    Added more null values & coalescing operator

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+#nullable enable
 
 namespace Chapter02
 {
+    class Address
+    {
+        public string? Building;
+        public string Street = string.Empty;
+        public string City = string.Empty;
+        public string Region = string.Empty;
+
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -57,7 +67,24 @@ namespace Chapter02
             thisCouldBeNull = 7;
             Console.WriteLine($"The value of thisCouldBeNull when set to 7 is: {thisCouldBeNull}\n");
 
+            var address = new Address();
+            address.Building = null;
+            address.Street = null;
+            address.City = "London";
+            address.Region = null;
 
+            string authorName = null;
+
+            //following throws a NullReferenceException
+            //int authorNameLength = authorName.Length;
+            //instead of throwing an exception, null is assigned to y
+            int? y = authorName?.Length;
+            Console.WriteLine($"Author name length is: {y}");
+
+            //the null coalescing operator (??)
+            //Result will be 3 if authorName?.Length is null
+            var result = authorName?.Length ?? 3;
+            Console.WriteLine("The coalesce operator has changed the author name length from null to: "+result);
         }
     }
 }
